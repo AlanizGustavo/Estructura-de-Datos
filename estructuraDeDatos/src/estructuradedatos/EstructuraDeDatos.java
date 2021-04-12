1d248cdeabf3258a23cf46a8e3d467e64c2d0c04
ALANIZ GUSTAVO <>
2021-04-12 19:49:55 -0300

    MENSAJE DE PRUEBA "HOLA MUNDO"

---
+++ estructuraDeDatos/src/estructuradedatos/EstructuraDeDatos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuradedatos;
+
+/**
+ *
+ * @author alanizgustavo
+ */
+public class EstructuraDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }
+    
+}
